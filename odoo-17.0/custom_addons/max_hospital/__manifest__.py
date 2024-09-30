4cecd631bc06c1beced1a6f7c7e865cfef137b0e
Simran Deep Kaur <simran@sutraatechno.com>
2024-09-30 10:19:57 +0530

    added new files

--- odoo-17.0/custom_addons/max_hospital/__manifest__.py
+++ odoo-17.0/custom_addons/max_hospital/__manifest__.py
@@ -5,14 +5,15 @@
     'version' : '17.0.1.0',
     'sequence' : -100,
     'depends' :[
-        'base_setup','base','digest','mail'
+        'mail',
         ],
     'data' : [
+        'max_hospital/security/ir.model.access.csv',
         'views/menu.xml',
         'views/female_patient_view.xml',
         'views/male_patient_view.xml',
         'views/appointment.xml',
-        'max_hospital/security/ir.model.access.csv',
+       
               ],
     'installable': True,
     'application': True,
